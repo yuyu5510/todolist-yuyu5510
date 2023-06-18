5ee7ae83258b3e451c861e1b8bbafa123bc0c127
yuyu5510 <yuyu5510.chara@icloud.com>
2023-06-18 12:32:31 +0900

    練習問題2の前まで

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -24,9 +24,10 @@ const addItem = () => {
   <div>
     <div>ItemList</div>
     <ul>
-      <li v-for="item in items" :key="item.name">
+      <li v-for="item in items" :key="item.name" :class="{ over500: item.price >= 500 }">
         <div>名前: {{ item.name }}</div>
         <div>{{ item.price }} 円</div>
+        <div v-if="item.price >= 10000">高額商品</div>
       </li>
     </ul>
     <div>
@@ -43,4 +44,8 @@ const addItem = () => {
   </div>
 </template>
 
-<style></style>
+<style>
+.over500 {
+  color: red;
+}
+</style>
